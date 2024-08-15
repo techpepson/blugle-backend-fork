9da42d5568f19870edc67e4dd241f6a9aa2b6e72
techpepson <ddpeprah@st.ug.edu.gh>
2024-08-15 02:45:27 +0000

    feat(update schema): make some of the schema details optional

--- server.js
+++ server.js
@@ -282,6 +282,16 @@ app.post("/api/book-appointment", async (req, res) => {
   }
 });
 
+//get users in the system
+
+app.get("/api/get-users", async (req, res) => {
+  try {
+    const users = await User.findOne({ userEmail });
+    users && res.status(200).json({ userData: users });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 //server port
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
