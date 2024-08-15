580ebc3460f5a34ccdb87913836643330244358d
techpepson <ddpeprah@st.ug.edu.gh>
2024-08-15 02:17:30 +0000

    feat(update schema): make some of the schema details optional

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.post("/api/login", async (req, res) => {
         hashedPassword
       );
 
-      if (comparePasswor userEmail) {
+      if (userEmail) {
         //get user email from the database
         const userDatabaseEmail = user.userEmail;
         //get user role
